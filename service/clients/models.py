72d6dede74a5504971f0b127e7ab2d302862f1c8
mivanov06 <mivanov06@yandex.ru>
2023-09-19 15:33:02 +1000

    edited models

--- service/clients/models.py
+++ service/clients/models.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Clients(models.Model):
+class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     company_name= models.CharField(max_length=100)
     full_address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.user.__str__()} - {self.company_name}'
--- service/services/models.py
+++ service/services/models.py
@@ -1,10 +1,13 @@
 from django.core.validators import MaxValueValidator
 from django.db import models
-from clients.models import Clients
+from clients.models import Client
 
 class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.name} ({self.full_price})'
 
 
 class Plan(models.Model):
@@ -19,8 +22,14 @@ class Plan(models.Model):
                                                        MaxValueValidator(100)
                                                    ])
 
+    def __str__(self):
+        return f'{self.plan_type} (-{self.discount_percent}%)'
+
 
 class Subscription(models.Model):
-    client = models.ForeignKey(Clients, related_name='subscriptions', on_delete=models.PROTECT)
+    client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.client} - {self.service.name}'
