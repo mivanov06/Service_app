8360378e4129a8f63722bbcd7b2f7931b1a9f59c
mivanov06 <mivanov06@yandex.ru>
2023-09-19 15:13:37 +1000

    created models for apps clients, services

--- service/clients/models.py
+++ service/clients/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Clients(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    company_name= models.CharField(max_length=100)
+    full_address = models.CharField(max_length=100)
